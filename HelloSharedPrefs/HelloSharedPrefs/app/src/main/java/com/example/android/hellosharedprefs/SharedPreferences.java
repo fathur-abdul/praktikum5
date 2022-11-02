ffadb84ada2b99ca2962d430e68044b3b78b2660
Farhan1654 <farhan1654akbar@gmail.com>
2022-11-02 10:54:02 +0700

    first commit

---
+++ HelloSharedPrefs/HelloSharedPrefs/app/src/main/java/com/example/android/hellosharedprefs/SharedPreferences.java
@@ -0,0 +1,17 @@
+package com.example.android.hellosharedprefs;
+
+public class SharedPreferences {
+    public Editor edit() {
+    }
+
+    public class Editor {
+        public void putInt(String count_key, int mCount) {
+        }
+
+        public void apply() {
+        }
+
+        public void clear() {
+        }
+    }
+}
